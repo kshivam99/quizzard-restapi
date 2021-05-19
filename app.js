d0188a9d48529380f61abad639c710b9f431dee8
kshivam99 <sawarn101@gmail.com>
2021-05-20 03:01:46 +0530

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const port = process.env.PORT || 3001;
+
+
+//middlewares
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Routes
+app.get('/', (req, res) => {
+    res.send("We are on home")
+})
+
+
+//connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, ()=>console.log("Connected to DB"))
+
+//Listening to the server
+app.listen(port, ()=>console.log(`Server started at http://localhost:${port}/`));
+
